8c64f13e72503cc9b22129940eec8b34a94d61e5
ismaelahmad6333 <106573961+ismaelahmad6333@users.noreply.github.com>
2022-12-05 08:38:55 +0000

    fix transition animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
-$(document).ready(function(){
-	$(window).scroll(function(){
-		if(this.scrollY > 20){
-			$('.navbar').addClass("sticky");
-		}
-		else{
-			$('.navbar').removeClass("sticky");
-		}
-	});
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass('sticky')
+    } else {
+      $('.navbar').removeClass('sticky')
+    }
+  })
 
-	$('.navbar .menu li a').click(function(){
-		$('html').css("scrollBehavior", "smooth");
-	});
+  $('.navbar .menu li a').click(function () {
+    $('html').css('scrollBehavior', 'smooth')
+  })
 
-	$('.menu-btn').click(function(){
-		$('.navbar .menu').toggleClass("active");
-		$('.menu-btn img').toggleClass("active");
-	});
-
-});
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass('active')
+    $('.menu-btn img').toggleClass('active')
+  })
+})
